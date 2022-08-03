f241c7452b1df95c6c345ee00e7dde7cff58b8eb
Timothy W <timothywee2@gmail.com>
2022-08-04 04:03:39 +0800

    Update optidoro_v4.py
    Trying to fix gspread_pandas module not found

--- optidoro_v4.py
+++ optidoro_v4.py
@@ -6,7 +6,7 @@ import pandas as pd
 from datetime import datetime
 # import numpy as np
 
-from gspread_pandas import Spread,Client
+from gspread-pandas import Spread,Client
 from google.oauth2 import service_account
 
 import random
